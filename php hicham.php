812b1cba44bce84267b8f20ac3e3ee3d33435f76
Hicham Houari <hichamhouarielmohamadi@alumno.ieselrincon.es>
2024-01-15 09:35:07 +0000

    He subido mi fichero php

---
+++ php hicham.php
@@ -0,0 +1,13 @@
+<?php
+$estudiante = array(
+    "nombre" => "Miguel santana",
+    "edad" => 12,
+    "direccion" => "Calle Miguel de cervante 50"
+);
+$estudiante ["edad"] ="16";
+$estudiante ["dirección"] = "Calle pepe 2";
+
+echo "Nombre" . $estudiante ["nombre"] . "\n";
+echo "Edad" . $estudiante  ["edad"] . "\n";
+echo "Dirección" . $estudiante ["dirección"] . "\n"; 
+?>
